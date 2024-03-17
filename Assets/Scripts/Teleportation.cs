35d42e40238554d57aa7abcf194310e3689065da
Ali Bennouna Louridi <96294983+Bouchta003@users.noreply.github.com>
2024-03-17 16:56:32 +0100

    teleportation when 3 potions are added

---
+++ Assets/Scripts/Teleportation.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+
+public class Teleportation : MonoBehaviour
+{
+    public Transform XR_Rig;
+
+    public Transform newPosition;
+
+    public KeyCode keyToPress = KeyCode.Space;
+
+    private XRGrabInteractable grabInteractable;
+
+    void Start()
+    {
+        XR_Rig.position = newPosition.position;
+    }
+
+    void Update()
+    {
+    }
+}
